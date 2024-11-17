bb7f7eeb8a01f878dbb8b2c77a300b13f2c8f0d6
Guilherme Franco <guilhermefrancodeoliveiraa@gmail.com>
2024-11-17 16:52:17 -0300

    first commit

---
+++ apideliveries/src/main/java/com/deliveries/apideliveries/ApideliveriesApplication.java
@@ -0,0 +1,13 @@
+package com.deliveries.apideliveries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApideliveriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApideliveriesApplication.class, args);
+	}
+
+}
---
+++ apideliveries/src/test/java/com/deliveries/apideliveries/ApideliveriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveries.apideliveries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApideliveriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
